35c8e0f40dde931339b1c9ab3190eac78644f380
KBandipo~ <keneebandipo@gmail.com>
2025-02-11 00:36:12 +0100

    refactor: update useGlobalState to allow undefined initial state

--- src/components/@hooks_state/useGlobal.tsx
+++ src/components/@hooks_state/useGlobal.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { createContainer } from 'unstated-next';
 
-function useGlobalState(initialState = { instanceId: '' }) {
-  const [instanceId, setInstanceId] = useState(initialState.instanceId);
+function useGlobalState(initialState: { instanceId: string } | undefined) {
+  const [instanceId, setInstanceId] = useState(initialState?.instanceId);
 
   const updateInstanceId = (id: string) => {
     setInstanceId(id);
