d8cbfec0b26204a488035e84f648674c6cff0392
KBandipo~ <keneebandipo@gmail.com>
2025-03-03 09:49:20 +0100

    feat: add templates.ts and implement issue templates fetching

---
+++ src/@core/templates.ts
@@ -0,0 +1,8 @@
+import { apiClient } from "./api-client";
+import { IssueTemplateResponse, IssueTemplate} from "./types/issueTemplate";
+
+export const getApiIssueTemplates = async (): Promise<IssueTemplate[]> => {
+    const response = await apiClient<IssueTemplateResponse>('/external/github/issue-templates', { method: 'GET' });
+    console.log('Issue Templates Data:', response.templates);
+    return response.templates;
+};
