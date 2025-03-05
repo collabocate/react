233d536d377265aea7e599d2a53c8e38c6ac724c
KBandipo~ <keneebandipo@gmail.com>
2025-03-05 10:24:52 +0100

    feat: add closeDropdown callback in fetchTemplateContent to close dropdown after fetching issue template

--- src/components/@hooks_state/useTemplate.ts
+++ src/components/@hooks_state/useTemplate.ts
@@ -2,25 +2,23 @@ import { useEffect, useState } from "react";
 import { createContainer } from 'unstated-next';
 import { IssueTemplate } from "../../@core/types/issueTemplate";
 import { getApiIssueTemplates } from "../../@core/templates";
-import { useDropdown } from "../@helpers/dropdown/useDropdown";
   
 function useTemplateState() {
 const [issueBody, setIssueBody] = useState<string>('');
 const [issueTitle, setIssueTitle] = useState<string>('');
 const [templates, setTemplates] = useState<IssueTemplate[]>([]);
-const {setIsOpen} = useDropdown();
 
 useEffect(() => {
     getApiIssueTemplates().then(setTemplates);
   }, []);
 
-const fetchTemplateContent = async (url: string) => {
+const fetchTemplateContent = async (url: string, closeDropdown: () => void) => {
     try {
       setIssueTitle('');
       const response = await fetch(url);
       const content = await response.text();
       setIssueBody(content);
-      setIsOpen(false)
+      closeDropdown()
     } catch (error) {
       console.log('Error fetching template content:', error);
     }
