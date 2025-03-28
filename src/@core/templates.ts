bbf551f4a379ee19d79a49d3f5931caa7a93a5dc
KBandipo~ <keneebandipo@gmail.com>
2025-03-28 23:39:27 +0100

    refactor: update issue templates endpoint path

--- src/@core/templates.ts
+++ src/@core/templates.ts
@@ -2,7 +2,7 @@ import { apiClient } from './api-client';
 import { IssueTemplateResponse, IssueTemplate} from './types/issueTemplate';
 
 export const getApiIssueTemplates = async (): Promise<IssueTemplate[]> => {
-    const response = await apiClient<IssueTemplateResponse>('/external/github/issue-templates', { method: 'GET' });
+    const response = await apiClient<IssueTemplateResponse>('/external/github/templates/issues', { method: 'GET' });
     console.log('Issue Templates Data:', response.templates);
     return response.templates;
 };
