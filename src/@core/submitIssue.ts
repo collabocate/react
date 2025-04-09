fd20c088b5e5d3b2b5f3a0b6f5240fce7880c066
KBandipo~ <keneebandipo@gmail.com>
2025-04-09 15:16:48 +0100

    Feat: Use Issue_POST_RequestBody for interface name

--- src/@core/submitIssue.ts
+++ src/@core/submitIssue.ts
@@ -1,7 +1,7 @@
 import { apiClient } from "./api-client";
-import { IssueData, IssueResponse } from "./types/issueData.types";
+import { Issue_POST_RequestBody, IssueResponse } from "./types/issueData.types";
 
-export const submitIssue = async (issueData: IssueData): Promise<IssueResponse> => {
+export const submitIssue = async (issueData: Issue_POST_RequestBody): Promise<IssueResponse> => {
     const response = await apiClient<IssueResponse>('/external/github/issues', {
       method: 'POST',
       body: JSON.stringify(issueData),
