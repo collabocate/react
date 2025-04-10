cbe8975d3beae8ad737b4790ea5c5bd923b2d780
KBandipo~ <keneebandipo@gmail.com>
2025-04-10 12:32:38 +0100

    feat: render issue messages using components instead of dangerouslySetInnerHTML

--- src/components/@hooks_state/useSubmitIssue.ts
+++ src/components/@hooks_state/useSubmitIssue.ts
@@ -5,7 +5,7 @@ import { GitHubIssueTemplateContainer } from './useTemplate';
 
 function useSubmitIssueState (){
   const { issueBody, setIssueBody, issueTitle, setIssueTitle } = GitHubIssueTemplateContainer.useContainer();
-  const [toastrMessage, setToastrMessage] = useState<string | null>(null);
+  const [toastrMessage, setToastrMessage] = useState<{message: string; issueURL?: string; issueNumber?: number;} | null>(null);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -13,7 +13,7 @@ function useSubmitIssueState (){
     const body = issueBody.trim();
     
     if (!title || !body) {
-      setToastrMessage('Issue Title and Body cannot be empty.');
+      setToastrMessage({message:'Issue Title and Body cannot be empty.'});
       return;
     }
     
@@ -23,13 +23,11 @@ function useSubmitIssueState (){
       console.log('Issue submitted:', response);
       setIssueTitle('');
       setIssueBody('');
-      
-      const issueTicketUrl = response.issue.html_url;
-      const issueNumber = response.issue.number;
-      
-      setToastrMessage(
-        `${response.message} Follow your issue ticket's progress here: <a target="_blank" href="${issueTicketUrl}">Issue ticket #${issueNumber}</a>`
-      );
+      setToastrMessage({
+        message: response.message,
+        issueURL: response.issue.html_url,
+        issueNumber: response.issue.number,
+      });
   };
 
   return {
