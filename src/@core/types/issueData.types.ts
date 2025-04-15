393a43564a4e4c29ce520b41a1ff63a6fed60c42
KBandipo~ <keneebandipo@gmail.com>
2025-04-16 00:12:55 +0100

    feat: define types for issue data and POST request

--- src/@core/types/issueData.types.ts
+++ src/@core/types/issueData.types.ts
@@ -4,18 +4,8 @@ export interface Issue_POST_RequestBody {
 }
 
 export interface IssueDetails {
-  repository_url: string;
-  html_url:string;
+  url:string;
   number: number;
-  id: number;
-  user: object;
-  assignee: any;
-  assignees: any[];
-  created_at: string;
-  updated_at: string;
-  closed_at: any,
-  author_association: string;
-  body:string;
 }
 
 export interface IssueResponse {
