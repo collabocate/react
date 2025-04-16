94f4f3e7ef15656c64fcf2ab8d4ad6818054f523
KBandipo~ <keneebandipo@gmail.com>
2025-04-17 00:21:22 +0100

    Feat: Display the issue post success message

--- src/components/@section_modal/ModalPopup.tsx
+++ src/components/@section_modal/ModalPopup.tsx
@@ -10,7 +10,7 @@ export const ModalPopup: React.FunctionComponent<ModalPopupProps> = (props: Moda
   const { instanceId, updateInstanceId } = GlobalContainer.useContainer();
   console.log('ModalPopup instance_id:', instanceId);
   const { issueBody, setIssueBody, issueTitle, setIssueTitle } = GitHubIssueTemplateContainer.useContainer();
-  const { toastrSuccessMessage, followIssueMessage, handleSubmit } = SubmitIssueContainer.useContainer();
+  const { toastrMessage, handleSubmit } = SubmitIssueContainer.useContainer();
 
   return (
     <>
@@ -43,21 +43,19 @@ export const ModalPopup: React.FunctionComponent<ModalPopupProps> = (props: Moda
             <textarea className="bb-content-group__collabocate_form-inner bb-collabocate_input bb-collabocate_textarea" id="issueBody" onChange={(e) =>setIssueBody(e.target.value)} value={issueBody}></textarea>
           </div>
           <button className="bb-content-group__collabocate_form-inner bb-collabocate_submit-form-button" id="submitIssueButton">Submit Issue Ticket</button>
-          {toastrSuccessMessage && (
-            <div id="displayToastrMessage">
-              {toastrSuccessMessage.message}
-            </div>
-          )}
-          {followIssueMessage && (
+          {toastrMessage && (
             <div>
-               {followIssueMessage.issueURL && followIssueMessage.issueNumber && (
-                <>
-                  Follow your issue ticket's progress here:{" "}
-                  <a href={followIssueMessage.issueURL} target="_blank" rel="noopener noreferrer">
-                    Issue ticket #{followIssueMessage.issueNumber}
-                  </a>
-                </>
-              )}
+              <div>{toastrMessage.message}</div>
+              <div>
+                {toastrMessage.issueURL && toastrMessage.issueNumber && (
+                  <>
+                    Follow your issue ticket's progress here:{" "}
+                    <a href={toastrMessage.issueURL} target="_blank" rel="noopener noreferrer">
+                      Issue ticket #{toastrMessage.issueNumber}
+                    </a>
+                  </>
+                )}
+              </div>
             </div>
           )}
         </form>
