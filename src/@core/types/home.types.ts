f14e94520b01528006e9c98a538d4f430dace2ea
KBandipo~ <keneebandipo@gmail.com>
2025-06-10 12:18:14 +0100

    refactor: restructure ApiHomeResponse to include success,message and data structure

--- src/@core/types/home.types.ts
+++ src/@core/types/home.types.ts
@@ -1,12 +1,16 @@
 export interface ApiHomeResponse {
-  name: string;
-  description: string;
-  built_at: string;
-  github: {
-    repository: string;
-  };
-  contributors: {
-    count: number;
-    list: string[];
-  };
+  success: string;
+  message: string;
+  data: {
+    name: string;
+    description: string;
+    built_at: string;
+    github: {
+      repository: string;
+    };
+    contributors: {
+      count: number;
+      list: string[];
+    };
+  }
 }
